75ae8c779f02b9c71949af7199c8dbecd327d329
Madison Grams <madisongrams.dev@gmail.com>
2024-04-11 12:20:00 -0400

    fixing whitespace

--- lunar/health_apis/health_apis/celery.py
+++ lunar/health_apis/health_apis/celery.py
@@ -3,8 +3,6 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
-from datetime import timedelta
-from django.db.models import Avg, Max, Min
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'health_apis.settings')
@@ -20,13 +18,14 @@ app.autodiscover_tasks()
 
 app.conf.timezone = 'UTC'
 
+# Celery beat schedule for the two tasks
 app.conf.beat_schedule = {
-  'get-altitude': {
-    'task': 'apis.tasks.get_altitude',
-    'schedule': 10.0,
-  },
-  'check-altitude': {
-    'task': 'apis.tasks.check_altitude',
-    'schedule': 60.0,
-  },
+    'get-altitude': {
+        'task': 'apis.tasks.get_altitude',
+        'schedule': 10.0,
+    },
+    'check-altitude': {
+        'task': 'apis.tasks.check_altitude',
+        'schedule': 60.0,
+    },
 }
